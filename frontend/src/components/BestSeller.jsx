be1d3f4255a1ca90a3bd8e17b2a696429267f462
mahboob alam <mahboob6550@gmail.com>
2026-01-17 03:10:17 +0530

    bottomBanner component done

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -9,7 +9,7 @@ const BestSeller = () => {
   return (
     <div className='mt-16'>
       <p className='text-2xl md:text-3xl font-medium'>Best Sellers</p>
-      <div className='grid grid-cols-2 sm:grid-cols-3 gap-3 md:grid-cols-4 md:gap-6 lg:grid-cols-5 '>
+      <div className='grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 sm:gap-4 md:gap-6 '>
         {
           product.filter((product)=>product.inStock).slice(0,5).map((product,index)=>(
             <ProductCard key={index} product={product }/>
