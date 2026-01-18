5302415b4a2714c0e593b333e695f5deb7cae24e
mahboob alam <mahboob6550@gmail.com>
2026-01-19 00:23:07 +0530

    NewLetter section added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import MainBanner from '../components/MainBanner';
 import Categories from '../components/Categories';
 import BestSeller from '../components/BestSeller';
 import BottomBanner from '../components/BottomBanner';
+import NewsLetter from '../components/NewsLetter';
 
 const Home = () => {
   return (
@@ -12,7 +13,7 @@ const Home = () => {
       <Categories />
       <BestSeller />
       <BottomBanner />
-
+      <NewsLetter />
     </div>
   );
 };
