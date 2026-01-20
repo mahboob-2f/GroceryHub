66891ee2bad0f031b06a469f84f625e6a623399c
mahboob alam <mahboob6550@gmail.com>
2026-01-20 21:03:11 +0530

    product category for group of products done with DYNAMIC ROUTING

--- frontend/src/Routees.jsx
+++ frontend/src/Routees.jsx
@@ -3,6 +3,7 @@ import { Routes ,Route } from 'react-router';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import AllProducts from './pages/AllProducts';
+import ProductCategory from './pages/ProductCategory';
 
 
 const Routees = () => {
@@ -10,6 +11,7 @@ const Routees = () => {
     <Routes>
         <Route path ='/' element={<Home/>}></Route>
         <Route path='/products' element={<AllProducts />}/>
+        <Route path='/products/:category' element={<ProductCategory /> } />
 
         <Route path='*' element={<NotFound/>}></Route>
     </Routes>
---
+++ frontend/src/pages/ProductCategory.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import { useParams } from 'react-router';
+import { categories } from '../assets/assets';
+import ProductCard from '../components/ProductCard';
+
+const ProductCategory = () => {
+
+    const {product}= useContext(AppContext);
+    const {category}= useParams();
+
+    const searchCategory= categories.find((item)=>item.path.toLowerCase()===category);
+    const fileteredProducts = product.filter((item)=>item.category.toLowerCase()===category);
+
+  return (
+    <div className='mt-16'>
+      {
+        searchCategory && 
+        <div className='flex flex-col items-end w-max'>
+            <p className='text-2xl font-medium'>{searchCategory.text.toUpperCase()}</p>
+            <div className='w-16 h-0.5 bg-primary rounded-full'></div>
+        </div>
+      }
+      {
+        fileteredProducts.length >0 ?
+        (
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md: gap-6 lg:grid-cols-5 mt-6'>
+                {
+                    fileteredProducts.map((_,index)=>(
+                        <ProductCard key={index} product={_} />
+                    ))
+                }
+            </div>
+        )
+        :
+        (
+            <div className='flex items-center justify-center h-[60vh]'>
+                <p className='text-2x1 font-medium text-primary'>No Products found in this category</p>
+            </div>
+        )
+      }
+    </div>
+  );
+};
+
+export default ProductCategory;
