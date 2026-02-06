077b2a7657093918b4eca5ffeeaf9c0c6917775c
mahboob alam <mahboob6550@gmail.com>
2026-02-07 04:14:52 +0530

    fix the my order page

--- frontend/src/pages/MyOrders.jsx
+++ frontend/src/pages/MyOrders.jsx
@@ -54,7 +54,7 @@ const MyOrders = () => {
                                 </div>
                                 <div className='flex flex-col justify-center md:ml-8 mb-4 md:mb-0'>
                                     <p>Quantity : {item.quantity} || "1"</p>
-                                    <p>Status : {item.status}</p>
+                                    <p>Status : {order.status}</p>
                                     <p>Date : {new Date(order.createdAt).toLocaleDateString()}</p>
                                 </div>
                                 <p className='text-primary text-lg font-medium'>Amount : {currency}{item.product.offerPrice * item.quantity}</p>
