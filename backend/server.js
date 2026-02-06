55f0d3e1c3e7f45599252ae6ffd5d65faf1b2a13
mahboob alam <mahboob6550@gmail.com>
2026-02-07 04:22:47 +0530

    added deployed frontend link to server file

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ await connectDB();
 await connectCloudinary();
 
 //  allow multiples origins
-const allowedOrigins=['http://localhost:5173']
+const allowedOrigins=['http://localhost:5173','https://grocery-hub-two.vercel.app']
 
 app.post('/stripe',express.raw({type: 'application/json'}),stripeWebHook)
 
