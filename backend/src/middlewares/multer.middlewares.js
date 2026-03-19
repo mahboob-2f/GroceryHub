bfce097fb46c8e291199d1c8daec83a2146b9de7
mahboob alam <mahboob6550@gmail.com>
2026-03-19 23:39:54 +0530

    fixing uploads directory for server for adding products from live link

--- backend/src/middlewares/multer.middlewares.js
+++ backend/src/middlewares/multer.middlewares.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/images/uploads')
+    cb(null, './uploads')
   },
   filename: function (req, file, cb) {
     crypto.randomBytes(12,(err,bytes)=>{
