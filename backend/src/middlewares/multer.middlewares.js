9e59094efeacf03e24ff5feb057b78ba485855bc
mahboob alam <mahboob6550@gmail.com>
2026-03-19 23:50:59 +0530

    fixing relaive path of uploads folder to absolute path

--- backend/src/middlewares/multer.middlewares.js
+++ backend/src/middlewares/multer.middlewares.js
@@ -2,12 +2,20 @@ import multer from "multer";
 import crypto from 'crypto'
 import path from 'path'
 
+
+
+const uploadPath = path.join(process.cwd(), "public", "images", "uploads");
+if (!fs.existsSync(uploadPath)) 
+    fs.mkdirSync(uploadPath, { recursive: true })
+
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads')
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     crypto.randomBytes(12,(err,bytes)=>{
+        if(err) return cb(err);
         const fileName = bytes.toString("hex")+path.extname(file.originalname);
         cb(null, fileName);
     })
