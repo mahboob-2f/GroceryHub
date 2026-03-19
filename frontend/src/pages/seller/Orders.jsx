e7854c6ed83a066fa1819f40fcf0dd41f657cf01
mahboob alam <mahboob6550@gmail.com>
2026-03-20 03:08:15 +0530

    order page of seller is updated ( will show loading page untill orders are not completely fetched and will show no orders if there were no orders yet)

--- frontend/src/pages/seller/Orders.jsx
+++ frontend/src/pages/seller/Orders.jsx
@@ -4,17 +4,21 @@ import { assets, dummyOrders } from '../../assets/assets';
 import toast from 'react-hot-toast';
 
 const Orders = () => {
-    const { currency ,axios } = useContext(AppContext);
+    const { currency, axios } = useContext(AppContext);
     const [orders, setOrders] = useState([])
+    const [loading, setLoading] = useState(true);
 
     const fetchOrders = async () => {
+        setLoading(true);
         try {
-            const {data}= await axios.get('/api/order/seller',{ withCredentials: true });
-            if(data.success) setOrders(data.orders);
+            const { data } = await axios.get('/api/order/seller', { withCredentials: true });
+            if (data.success) setOrders(data.orders);
             else toast.error(data.message);
         } catch (error) {
             toast.error(error.message);
-            
+        }
+        finally {
+            setLoading(false);
         }
     }
     useEffect(() => {
@@ -22,42 +26,70 @@ const Orders = () => {
     }, [])
 
     return (
-        <div className='no-scrollbar flex-1 h-[95vh] overflow-scroll '>
-            <div className="md:p-10 p-4 space-y-4">
-                <h2 className="text-lg font-medium">Orders List</h2>
-                {orders.map((order, index) => (
-                    <div key={index} className="flex flex-col md:flex-row justify-between md:items-center gap-5 p-5 max-w-4xl rounded-md border border-gray-300 ">
-                        <div className="flex gap-5 w-80">
-                            <img className="w-12 h-12 object-cover  " src={assets.box_icon} alt="boxIcon" />
-                            <div>
-                                {order.items.map((item, index) => (
-                                    <div key={index} className="flex flex-col    ">
-                                        <p className="font-medium">
-                                            {item.product.name} {" "} <span className='text-primary'>x {item.quantity}</span>
-                                        </p>
-                                    </div>
-                                ))}
-                            </div>
+        <>
+            {
+                loading ?
+                    (
+                        <div className='border-2 border-gray-300 rounded-lg p-8 py-10 text-center bg-white text-gray-500/70'>
+                            <p className='text-xl font-bold text-gray-700 mb-2'>Loading Orders...</p>
+                            <p className='text-sm'>Please wait while your orders are being fetched.</p>
                         </div>
+                    )
+                    :
+                    (
+                        <div>
+                            {
+                                orders.length > 0 ?
+                                    (<div className='no-scrollbar flex-1 h-[95vh] overflow-scroll '>
+                                        <div className="md:p-10 p-4 space-y-4">
+                                            <h2 className="text-lg font-medium">Orders List</h2>
+                                            {orders.map((order, index) => (
+                                                <div key={index} className="flex flex-col md:flex-row justify-between md:items-center gap-5 p-5 max-w-4xl rounded-md border border-gray-300 ">
+                                                    <div className="flex gap-5 w-80">
+                                                        <img className="w-12 h-12 object-cover  " src={assets.box_icon} alt="boxIcon" />
+                                                        <div>
+                                                            {order.items.map((item, index) => (
+                                                                <div key={index} className="flex flex-col    ">
+                                                                    <p className="font-medium">
+                                                                        {item.product.name} {" "} <span className='text-primary'>x {item.quantity}</span>
+                                                                    </p>
+                                                                </div>
+                                                            ))}
+                                                        </div>
+                                                    </div>
 
-                        <div className="text-sm md:text-base text-black/60">
-                            <p className='text-black/80'>{order.address.firstName}, {order.address.lastName}</p>
-                            <p>{order.address.street}, {order.address.city}</p> <p> {order.address.state}, {order.address.zipcode}, {order.address.country}</p>
-                            <p></p>
-                            <p>{order.address.phone}</p>
+                                                    <div className="text-sm md:text-base text-black/60">
+                                                        <p className='text-black/80'>{order.address.firstName}, {order.address.lastName}</p>
+                                                        <p>{order.address.street}, {order.address.city}</p> <p> {order.address.state}, {order.address.zipcode}, {order.address.country}</p>
+                                                        <p></p>
+                                                        <p>{order.address.phone}</p>
+                                                    </div>
+
+                                                    <p className="font-medium text-lg my-auto ">{currency}{order.amount}</p>
+
+                                                    <div className="flex flex-col text-sm md:text-black text-black/60 ">
+                                                        <p>Method: {order.paymentType}</p>
+                                                        <p>Date: {new Date(order.createdAt).toLocaleDateString()}</p>
+                                                        <p>Payment: {order.isPaid ? "Paid" : "Pending"}</p>
+                                                    </div>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>)
+                                    :
+                                    (
+                                        <div className='border-2 border-gray-300 rounded-lg p-8 py-10 text-center bg-white text-gray-500/70'>
+                                            <p className='text-xl font-bold text-gray-700 mb-2'>No Orders Found</p>
+                                            <p className='text-sm'>You haven’t any orders yet.</p>
+                                        </div>
+                                    )
+                            }
+
+
                         </div>
-
-                        <p className="font-medium text-lg my-auto ">{currency}{order.amount}</p>
-
-                        <div className="flex flex-col text-sm md:text-black text-black/60 ">
-                            <p>Method: {order.paymentType}</p>
-                            <p>Date: {new Date(order.createdAt).toLocaleDateString()}</p>
-                            <p>Payment: {order.isPaid ? "Paid" : "Pending"}</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </div>
+                    )
+            }
+        </>
 
     );
 };
